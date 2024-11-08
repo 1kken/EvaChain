0a86649d22465254c73f911a096182ea33474f8b
ikkenzi <tavorearljohn@gmail.com>
2024-11-08 11:23:59 +0800

    setup frontend dependencies

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+	let state = $state(0);
+
+	function increment() {
+		state++;
+	}
+</script>
+
+<Button onclick={increment}>Hello World! {state}</Button>
