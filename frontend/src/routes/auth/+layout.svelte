bc028c407ee19a1b04b292a309b63fe0bc8b0e1a
ikkenzi <tavorearljohn@gmail.com>
2024-11-15 00:58:35 +0800

    lets work on RBAC tommmorow

--- frontend/src/routes/auth/+layout.svelte
+++ frontend/src/routes/auth/+layout.svelte
@@ -1,8 +1,7 @@
-<script>
-	import Navbar from '$lib/components/custom/navbar.svelte';
-	import Footer from '$lib/components/custom/footer.svelte';
+<script lang="ts">
+	let { children } = $props();
 </script>
 
 <div class="relative flex h-screen w-screen items-center justify-center">
-	<slot />
+	{@render children()}
 </div>
