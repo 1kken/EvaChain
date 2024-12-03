23dcdce19d4282fc2613f212db6f6182bd80db9d
ikkenzi <tavorearljohn@gmail.com>
2024-12-03 14:39:43 +0800

    fix center:

--- frontend/src/routes/(app)/+page.svelte
+++ frontend/src/routes/(app)/+page.svelte
@@ -1,8 +1,10 @@
-<h1
-	class="mb-4 bg-gradient-to-r from-green-600 to-emerald-900 bg-clip-text stroke-emerald-900 text-4xl font-bold text-transparent md:text-6xl"
->
-	EvaChain
-</h1>
-<p class="text-muted-foreground max-w-2xl text-lg md:text-xl">
-	A blockchain based performance management system
-</p>
+<div class="text-center">
+	<h1
+		class="mb-4 bg-gradient-to-r from-green-600 to-emerald-900 bg-clip-text stroke-emerald-900 text-4xl font-bold text-transparent md:text-6xl"
+	>
+		EvaChain
+	</h1>
+	<p class="text-muted-foreground max-w-2xl text-lg md:text-xl">
+		A blockchain based performance management system
+	</p>
+</div>
