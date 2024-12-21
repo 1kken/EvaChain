75d2589dfe8181665919d28cdf5368c3b864bece
ikkenzi <tavorearljohn@gmail.com>
2024-12-22 02:29:28 +0800

    hehe

--- frontend/src/routes/(dashboard)/dashboard/(dashboard)/operational_plan/[opid]/components/Header.svelte
+++ frontend/src/routes/(dashboard)/dashboard/(dashboard)/operational_plan/[opid]/components/Header.svelte
@@ -5,11 +5,11 @@
 <div class="bg-background/50 backdrop-blur">
 	<div class="space-y-1">
 		<div transition:fly={{ y: -20, duration: 400 }}>
-			<h3 class="text-xl font-medium">Program Management</h3>
+			<h3 class="text-xl font-medium">Operational Plan</h3>
 		</div>
 		<div transition:fade={{ duration: 400 }}>
 			<p class="text-muted-foreground text-sm">
-				Manage your programs, objectives, and activities efficiently
+				Manage objective,activities, budget management, and quarterly assignment
 			</p>
 		</div>
 	</div>
